1fc6f477427d2ce87e3c5652fd8477914936f91e
chrisakhula <ouma.cleo@gmail.com>
2024-03-16 16:23:57 +0300

    Personal information

--- program1..dart
+++ program1..dart
@@ -1,2 +1,11 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//## Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main() {
+  String name = "Cleophas";
+  int age = 27;
+  String school = "Zetech University Alumini";
+  String hobby = "Nature Walks";
+
+  print("My name is $name, I am $age years old, I am a $school graduted in 2022 and I love $hobby");
+}
